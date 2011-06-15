450f9d61c838471875e7ebbaa67c84f36f984a4b
Micah Woods <micahwoods@gmail.com>
2011-06-15 18:40:46 -0500

    Rails App Should Be Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomodoro::Application.config.secret_token = 'f2a0d99174ab13eb00bc8c896aa3858ce228e3bd819fa7d25b996da5b402695937cd0b84cc0da8e6068b064378573c74290fa3c403cc5db1d0027396d2d75918'
